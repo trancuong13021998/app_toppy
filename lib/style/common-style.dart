e700524c1eafc107abe24be51de1195d919f27bc
Tran Cuong <5562274-cuongtt@users.noreply.gitlab.com>
2020-12-10 09:20:48 +0700

    Update home screen

--- lib/style/common-style.dart
+++ lib/style/common-style.dart
@@ -6,17 +6,18 @@ abstract class CommonStyle {
   static Color colorNavigationBottom = const Color(0xffC7C7C7);
   static Color colorGrey = Color(0xffE5E5E5);
   static Color colorBlue = Color(0xffB5DAFF);
-  static Color colorBackground = Color(0xffF1F1F1);
+  static Color colorBackground = Color(0xffF9F9F9);
   static Color colorGreyText = Color(0xff6E7985);
   static Color colorBlackText = Color(0xff253647);
   static Color colorPink = Color(0xffF8D6DF);
   static Color redColor = Color(0xFFD62C2C);
   static Color darkBlueColor = Color(0xFF0045BB);
-
+  static Color colorBoxShadow = Color(0xFFDDE5F2);
   static BoxShadow defaultShadow = BoxShadow(
-    offset: Offset(10, 5),
-    blurRadius: 27,
-    color: Colors.black12, // Black color with 12% opacity
+    color: CommonStyle.colorBoxShadow,
+    spreadRadius: 0,
+    blurRadius: 20,
+    offset: Offset(0,5), // changes position of shadow
   );
 
   static TextStyle textBoldSize_12 = TextStyle(
